42b39cca8e01ee42444e377533a68dffaae0c500
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-10-25 19:16:17 +0200

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 
 const Home = () => {
     const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
         setName('luigi');
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>{ name } is { age }</p>
             
             <button onClick={handleClick}>Click me</button>
         </div>
